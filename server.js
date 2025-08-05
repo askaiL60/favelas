be63651a1535b1f541d44c1c02b8b1e54318cc71
askaiL60 <152877179+askaiL60@users.noreply.github.com>
2025-08-05 08:22:42 +0300

    Modification server.js pour Railway
    connexion MySQL

--- server.js
+++ server.js
@@ -8,12 +8,16 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+require('dotenv').config();
+
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'formulairedb'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
+
 
 // Route POST : inscription avec vérif pseudo unique
 app.post('/submit', (req, res) => {
